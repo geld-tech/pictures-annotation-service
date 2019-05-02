98c32b561bae625972dcb45ffd63bca671a3a3c1
zlig <gdevillard@gmail.com>
2019-05-02 21:02:29 +0100

    Formatting uploaded files names

--- sources/webapp/src/components/Index.vue
+++ sources/webapp/src/components/Index.vue
@@ -22,7 +22,14 @@
                           v-model="file" v-bind:state="Boolean(file)"
                           accept="image/*"
                           placeholder="Choose a file..."
-                          drop-placeholder="Drop file here..."></b-form-file>
+                          drop-placeholder="Drop file here...">
+                           <template slot="file-name" slot-scope="{ names }">
+                             <b-badge variant="dark">{{ names[0] }}</b-badge>
+                             <b-badge v-if="names.length > 1" variant="dark" class="ml-1">
+                               + {{ names.length - 1 }} More files
+                             </b-badge>
+                           </template>
+                        </b-form-file>
                         <div class="mt-3">Selected file: {{ file ? file.name : '' }}</div>
                     </div>
                 </b-col>
