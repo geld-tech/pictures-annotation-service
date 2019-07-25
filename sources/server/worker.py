1e3158f608320d7a0900a3bb7053e0e4efb88ed1
zlig <gdevillard@gmail.com>
2019-07-25 20:41:41 +0100

    Sorted imports

--- sources/server/worker.py
+++ sources/server/worker.py
@@ -7,10 +7,9 @@ import os
 import socket
 
 from celery import Celery, states
+from modules.Models import Base, Picture, Server
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-from modules.Models import Base, Picture, Server
 
 # Global variables
 local_path = os.path.dirname(os.path.abspath(__file__))
