ea92e694a37b891355f7dad910c179fcfa8178e2
zlig <gdevillard@gmail.com>
2019-08-06 18:20:48 +0100

    Design

--- sources/webapp/src/components/Index.vue
+++ sources/webapp/src/components/Index.vue
@@ -39,8 +39,6 @@
                             <ul>
                               <li v-for="(result, index) in results" v-bind:key="index">
                                 <b-img v-bind:src="'results/' + result.task_id + '/' + result.filename" v-bind:alt="result.filename" fluid center></b-img>
-                                <br />
-                                <strong>{{ result.filename }}</strong>
                               </li>
                             </ul>
                         </p>
