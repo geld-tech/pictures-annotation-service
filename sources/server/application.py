7be1a47dd136f42a133f7b85d2a92b64be6cbce7
zlig <gdevillard@gmail.com>
2019-08-06 17:56:20 +0100

    WIP

--- sources/server/application.py
+++ sources/server/application.py
@@ -349,8 +349,6 @@ def tasks():
 
 @app.route('/results/<path:path>/<path:filename>')
 def send_results(path, filename):
-    print "%s" % path
-    print "%s/data/%s" % (local_path, filename)
     return send_from_directory(local_path + '/data', filename)
 
 
