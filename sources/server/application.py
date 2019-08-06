ad5c37b31113a4117a187c82dfcba42873260106
zlig <gdevillard@gmail.com>
2019-08-06 18:11:59 +0100

    Syntax mistake

--- sources/server/application.py
+++ sources/server/application.py
@@ -354,7 +354,7 @@ def tasks():
 
 @app.route('/results/<path:path>/<path:filename>')
 def send_results(path, filename):
-    results_path = local_path + '/results/' + path, filename
+    results_path = local_path + '/results/' + path
     return send_from_directory(results_path, filename)
 
 
