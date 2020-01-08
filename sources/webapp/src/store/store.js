a936ef05425e518e039e169916d754b3f3edbbf4
zlig <gdevillard@gmail.com>
2020-01-08 12:41:17 +0000

    Loop-less

--- sources/webapp/src/store/store.js
+++ sources/webapp/src/store/store.js
@@ -34,21 +34,17 @@ export const store = new Vuex.Store({
   actions: {
     async getStatus({ commit }, payload) {
       console.log('Starting '+store.getters.taskStatus)
-      while (store.getters.taskStatus != 'COMPLETE' && store.getters.taskStatus != 'FAILED') {
-        console.log('Looping '+store.getters.taskStatus)
-        getTaskStatus(payload.taskId)
-          .then(response => {
-            console.log('RESPONSE XXX')
-            console.log('RESPONSE '+response.data)
-            commit('setTaskId', payload.taskId)
-            commit('setTaskStatus', response.data.status)
-            commit('setTaskResults', response.data.pictures)  /* XXX FIXME BUG Change in API*/
-          })
-          .catch(err => {
-            console.log('ERROR '+err.message)
-          })
-        new Promise(resolve => setTimeout(resolve, 250))
-      }
+      getTaskStatus(payload.taskId)
+        .then(response => {
+          console.log('RESPONSE XXX')
+          console.log('RESPONSE '+response.data)
+          commit('setTaskId', payload.taskId)
+          commit('setTaskStatus', response.data.status)
+          commit('setTaskResults', response.data.pictures)  /* XXX FIXME BUG Change in API*/
+        })
+        .catch(err => {
+          console.log('ERROR '+err.message)
+        })
     }
   }
 })
