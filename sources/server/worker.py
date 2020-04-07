bb35c7f9f241ca92de73c675b8f1aff0f51ca66b
zlig <gdevillard@gmail.com>
2020-04-07 15:50:41 +0100

    Linter

--- sources/server/worker.py
+++ sources/server/worker.py
@@ -100,8 +100,8 @@ def list():
 def identify_picture(filename):
     ''' Identify picture provided '''
     global config_file
-    
     settings = {'model': {'file': 'model.h5'}}
+
     if os.path.isfile(config_file):
         config = ConfigParser.ConfigParser()
         config.readfp(open(config_file))
